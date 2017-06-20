65bb44f855e90daf935a42cb1d930115ed862c30
devvtms <devvtms+git@gmail.com>
2017-06-20 11:56:58 +0200

    - comments

--- src/components/email-sign-in/email-sign-in.ts
+++ src/components/email-sign-in/email-sign-in.ts
@@ -4,28 +4,37 @@ import { AuthProvider } from '../../providers/auth/auth';
 import { ToastController } from 'ionic-angular';
 
 /**
- * Generated class for the EmailSignInComponent component.
- *
- * See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
- * for more info on Angular Components.
+ * 
+ * Component for email sign in
+ * 
+ * @export
+ * @class EmailSignInComponent
  */
 @Component({
   selector: 'sign-in',
   templateUrl: 'email-sign-in.html',
   providers: [FormBuilder]
 })
+
 export class EmailSignInComponent {
 
   public signInForm: FormGroup;
   public title = 'Sign in with email'
 
   constructor(private formBuilder: FormBuilder, public auth: AuthProvider, public toastCtrl: ToastController) {
+
+    // building the form
     this.signInForm = formBuilder.group({
       email: ['', Validators.compose([Validators.required, Validators.email])],
       password: ['', Validators.compose([Validators.minLength(6), Validators.required])]
     });
   }
 
+  /**
+   * Toast creator
+   * 
+   * @param message
+   */
   createToast(message: string) {
     return this.toastCtrl.create({
       message,
@@ -35,15 +44,25 @@ export class EmailSignInComponent {
 
   signInFormSubmit() {
 
+    // first we check, if the form is valid
     if (!this.signInForm.valid) {
-      this.createToast('Form not valid').present();
+      this.createToast('Ooops, form not valid...').present();
       return
-    }
-    else {
+    } else {
+
+      // if the form is valid, we continue with validation
       this.auth.signInUser(this.signInForm.value.email, this.signInForm.value.password)
         .then(() => {
           this.createToast('Signed in with email: ' + this.signInForm.value.email).present()
         },
+
+        /**
+         * when error appears in valiation, we show the error message. 
+         * Here you can customise messages about auth, when you check error message props:
+         * if (error.message = '') {}
+         * 
+         * @todo add examples
+         */
         (error) => {
           this.createToast(error.message).present();
         })
