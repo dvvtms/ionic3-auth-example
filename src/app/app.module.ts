70fb970cb155b1350f87edaa4321eb96b7dec7ee
devvtms <devvtms+git@gmail.com>
2017-06-20 11:42:59 +0200

    - comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,41 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
-import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
+import { MyApp } from './app.component';
+
+// angularfire
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireModule } from 'angularfire2';
+import { AuthProvider } from '../providers/auth/auth';
 
-import { MyApp } from './app.component';
+// pages
 import { HomePage } from '../pages/home/home';
 import { AuthPage } from '../pages/auth/auth';
+
+// auth components
 import { EmailSignInComponent } from '../components/email-sign-in/email-sign-in';
 import { EmailSignUpComponent } from '../components/email-sign-up/email-sign-up';
-import { AuthProvider } from '../providers/auth/auth';
+
 
 @NgModule({
   declarations: [
     MyApp,
+
+    // pages
     HomePage,
     AuthPage,
+
+    // components
     EmailSignInComponent,
     EmailSignUpComponent
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+
+    // auth/db modules
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFireDatabaseModule
@@ -34,8 +45,12 @@ import { AuthProvider } from '../providers/auth/auth';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+
+    // pages
     HomePage,
     AuthPage,
+
+    //components
     EmailSignInComponent,
     EmailSignUpComponent
   ],
@@ -43,6 +58,8 @@ import { AuthProvider } from '../providers/auth/auth';
     StatusBar,
     SplashScreen,
     { provide: ErrorHandler, useClass: IonicErrorHandler },
+
+    // auth provider
     AuthProvider
   ]
 })
